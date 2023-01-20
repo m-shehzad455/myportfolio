18d4bfce8a77cd071074c39ac7b520aa7b46933c
m-shehzad455 <shehzuali5@gmail.com>
2023-01-20 18:36:36 +0500

    update navbar and header section

---
+++ src/components/Header.js
@@ -0,0 +1,73 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
+
+const Header = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => setToggle(!toggle);
+
+  return (
+    <header className="flex justify-between px-5 py-3 bg-white text-black fixed w-full z-10">
+      <a href="/" className="logo text-2xl font-bold text-accent">
+        Shehzad Hussain
+      </a>
+
+      {/* Desktop Nav */}
+      <nav className="hidden md:block">
+        <ul className="flex">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="/#about">About</a>
+          </li>
+          <li>
+            <a href="/#projects">Projects</a>
+          </li>
+          <li>
+            <a href="/#blog">Blog</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
+          <li>
+            <a href="https://docs.google.com/document/d/1t4cMss_Zgn1tVx3ArE-IoCf7u0KY9FSfeins6Ij0USw/edit?usp=sharing" target="_blank" without rel="noreferrer">
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Mobile Nav */}
+      <nav
+        className={!toggle ? "mobile-nav left-[-100%]" : "mobile-nav left-0"}
+      >
+        <ul className="flex flex-col">
+          <li>
+            <a href="/#about">About</a>
+          </li>
+          <li>
+            <a href="/#projects">Projects</a>
+          </li>
+          <li>
+            <a href="/#blog">Blog</a>
+          </li>
+          <li>
+            <a href="/#contact">Contact</a>
+          </li>
+          <li>
+            <a href="/#resume">Resume</a>
+          </li>
+        </ul>
+      </nav>
+
+      {/* Toggle button */}
+      <button onClick={handleToggle} className="block md:hidden">
+        {!toggle ? <AiOutlineMenu size={30} /> : <AiOutlineClose size={30} />}
+      </button>
+    </header>
+  );
+};
+
+export default Header;
